8e2bfc5e73d452b9f684231c77f7f2602641ade2
infeng <fzhihao@outlook.com>
2017-05-24 10:36:24 +0800

    unuse style in Todo

--- src/components/Todo/index.tsx
+++ src/components/Todo/index.tsx
@@ -20,10 +20,6 @@ export default class Todo extends React.Component<Props, any> {
     return (
       <li className={className}
       onClick={this.props.onClick}
-      style={{
-        textDecoration: this.props.completed ? 'line-through' : 'none',
-        cursor: this.props.completed ? 'default' : 'pointer',
-      }}
       >
         {this.props.text}
       </li>
