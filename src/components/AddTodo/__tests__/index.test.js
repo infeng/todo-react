da337ae93c6400acbeebfee32caedd80d3cafaf0
infeng <fzhihao@outlook.com>
2017-05-24 15:45:13 +0800

    update AddTodo test

--- src/components/AddTodo/__tests__/index.test.js
+++ src/components/AddTodo/__tests__/index.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, mount } from 'enzyme';
+import { render, mount, shallow } from 'enzyme';
 import AddTodo from '..';
 
 function createAddTodo(props) {
@@ -12,17 +12,47 @@ describe('AddTodo', () => {
   it('render AddTodo correctly', () => {
     let addTodo = createAddTodo();
 
-    let wrapper = render(addTodo);
-    expect(wrapper).toMatchSnapshot();
+    let wrapper1 = shallow(addTodo);
+    expect(wrapper1.find('input').exists());
+
+    let wrapper2 = render(addTodo);
+    expect(wrapper2).toMatchSnapshot();
   });
   
-  it('click', () => {
+  it('onAddClick() shoule be called after input text', () => {
     let onAddClick = jest.fn();
     let addTodo = createAddTodo({onAddClick});
 
     let wrapper = mount(addTodo);
-    wrapper.find('input').node.value = '233';  
-    wrapper.find('button').simulate('click');
+    let input = wrapper.find('input');
+    let button = wrapper.find('button');
+
+    input.node.value = '233';  
+    button.simulate('click');
     expect(onAddClick).toBeCalledWith('233');
+  });
+
+    it('clear input value after add todo', () => {
+    let onAddClick = jest.fn();
+    let addTodo = createAddTodo({onAddClick});
+
+    let wrapper = mount(addTodo);
+    let input = wrapper.find('input');
+    let button = wrapper.find('button');
+
+    input.node.value = '233';  
+    button.simulate('click');
+    expect(input.node.value).toBe('');
+  });
+
+  it('onAddClick() shoule not be called whihout input text', () => {
+    let onAddClick = jest.fn();
+    let addTodo = createAddTodo({onAddClick});
+
+    let wrapper = mount(addTodo);
+    let button = wrapper.find('button');
+
+    button.simulate('click');
+    expect(onAddClick).not.toBeCalled();
   });
 });
